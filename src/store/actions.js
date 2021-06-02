3bded70d0a30197daa60d9f9188a0f49e0927a31
Sohail <lancerf505@gmail.com>
2021-06-02 20:48:18 +0500

    first commit

---
+++ src/store/actions.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+export const API_CALL = 'API_CALL'
+export const SET_GIT_USERS_DETAILS = 'SET_GIT_USERS_DETAILS'
+
+export const apiCall = () =>{
+    return{
+        type: API_CALL
+    }
+}
+
+export const fetchGitHubUsers = () =>{
+    return dispatch =>{
+        axios.get('https://api.github.com/users/example')
+        .then(response=>{
+            const users = response.data
+            dispatch(setGitUserDetails(users));
+            console.log(users)
+        })
+        .catch(error =>{
+            const errmessage = error.message
+            console.log(errmessage)
+        })
+    }
+}
+export const setGitUserDetails=(users)=> {
+    return {
+      type: SET_GIT_USERS_DETAILS,
+      payload: users,
+    };
+  }
